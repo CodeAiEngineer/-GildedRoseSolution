f966fddd980ce0dec49fea146977d13d7b572b05
KingCobraSnake <kalinlique35@hotmail.com>
2022-10-30 20:48:06 +0300

    Update gilded-rose.spec.ts

--- test/mocha/gilded-rose.spec.ts
+++ test/mocha/gilded-rose.spec.ts
@@ -1,10 +1,27 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '@/gilded-rose';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe('gildedRoseInventoryTest', () => {
+  it('The Quality of an item is never more than 50', () => {
+    const gildedRose = new GildedRose([new Item('randomitem', 0, 80)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+    //expect(items[0].name).to.equal('fixme');
+    expect(items[0].quality).to.equal(50);
   });
+
+  it('Sulfuras" is a legendary item and as such its Quality is 80 and it never alters', () => {
+    const gildedRose = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", -1, 55)]);
+    const items = gildedRose.updateQuality();
+    //expect(items[0].name).to.equal('fixme');
+    expect(items[0].quality).to.equal(80);
+
+
+  });
+
+
+
+
+
+
+
 });
