bc1d36e0730c36b730f05c072b37ecf9f223f9fa
KingCobraSnake <kalinlique35@hotmail.com>
2022-10-30 19:37:01 +0300

    Update gilded-rose.ts

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -75,8 +75,11 @@ export class GildedRose {
       
       //MY ADDITION: "Sulfuras" quality constraint added.
       if (this.items[i].name.includes('Sulfuras') ==true) {
-        this.items[i].quality=80
-      }    
+        this.items[i].quality= 80}
+      if ((this.items[i].name.includes('Sulfuras') ==false) && (this.items[i].quality > 50)) {
+        this.items[i].quality= 50}  
+
+          
     }
     
 
